e224cbf61faced0294d13c7725f1d168371232bd
AliNajafi1998 <najafi1998ali@gmail.com>
2022-08-04 12:31:13 +0300

    refactored

--- Filterings.py
+++ Filterings.py
@@ -1,14 +1,8 @@
-from typing import List
 import emoji
-
-from nltk.stem import PorterStemmer
-from nltk.stem.snowball import SnowballStemmer
-from nltk.stem import WordNetLemmatizer
 
 from urlextract import URLExtract
 import urllib
 import re
-import nltk
 
 
 class EmojiHandler:
@@ -75,37 +69,3 @@ class MentionHandler:
                 return re.sub(pattern, "\\1 \\2", text)
             else:
                 return re.sub(pattern, "\\2", text)
-
-
-class NLTKStopwordHandler:
-    def __init__(self, additional_stopwords: List[str]) -> None:
-        nltk.download('stopwords')
-        self.stopwords = nltk.corpus.stopwords.words('english')
-        self.stopwords.extend(additional_stopwords)
-
-    def __call__(self, tokens: List[str]) -> str:
-        return [token for token in tokens if token not in self.stopwords]
-
-
-class NLTLPorterStemmer:
-    def __init__(self) -> None:
-        self.stemmer = PorterStemmer()
-
-    def __call__(self, tokens: List[str]) -> str:
-        return [self.stemmer.stem(token) for token in tokens]
-
-
-class NLTKSNowBallStemmer:
-    def __init__(self, language="english") -> None:
-        self.stemmer = SnowballStemmer(language, ignore_stopwords=True)
-
-    def __call__(self, tokens: List[str]) -> str:
-        return [self.stemmer.stem(token) for token in tokens]
-
-
-class NLTKWordNetLemmatizer:
-    def __init__(self) -> None:
-        self.lemmatizer = WordNetLemmatizer()
-
-    def __call__(self, tokens: List[str]) -> str:
-        return [self.lemmatizer.lemmatize(token) for token in tokens]
