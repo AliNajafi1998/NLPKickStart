4043b730cf4389df4b1eef414468b9cde832d8b0
AliNajafi1998 <najafi1998ali@gmail.com>
2022-08-12 16:26:23 +0300

    refactoring done

--- Filterings.py
+++ Filterings.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import emoji
 import string
 from urlextract import URLExtract
@@ -11,23 +12,25 @@ class EmojiHandler:
 
 
 class EmailHandler:
-    def __init__(self) -> None:
+    def __init__(self, repl: str = ' ') -> None:
         self.pattern = r'[\w.+-]+@[\w-]+\.[\w.-]+'
+        self.repl = repl
 
-    def __call__(self, text: str, repl: str = ' ') -> str:
+    def __call__(self, text: str) -> str:
         match = re.findall(self.pattern, text)
         for m in match:
-            text = text.replace(m, repl).strip()
+            text = text.replace(m, self.repl).strip()
         return text
 
 
 class URLHandler:
-    def __init__(self) -> None:
+    def __init__(self, keep: bool = True) -> None:
         self.url_extractor = URLExtract()
+        self.keep = keep
 
-    def __call__(self, text: str, keep: bool = True) -> str:
+    def __call__(self, text: str) -> str:
         urls = list(self.url_extractor.gen_urls(text))
-        if keep == True:
+        if self.keep == True:
             updated_urls = [
                 url if 'http' in url else f'https://{url}' for url in urls]
             domains = [urllib.parse.urlparse(
@@ -37,43 +40,52 @@ class URLHandler:
             return text
         else:
             for i in range(len(urls)):
-                text = text.replace(urls[i], ' ').strip()
+                text = text.replace(urls[i], '').strip()
             return text
 
 
 class HashTagHandler:
+    def __init__(self, keep: bool = True, do_camel_case_split: bool = True) -> None:
+        self.keep = keep
+        self.do_camel_case_split = do_camel_case_split
+
     def camel_case_split(self, text):
         pattern = '.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)'
         matches = re.finditer(pattern, text)
         return " ".join([m.group(0) for m in matches])
 
-    def __call__(self, text: str, keep: bool = True, camel_case_split: bool = True) -> str:
+    def __call__(self, text: str) -> str:
         pattern = "(#([^\s]+))"
-        if keep == True and camel_case_split == False:
+        if self.keep == True and self.do_camel_case_split == False:
             return re.sub(pattern, "\\1 \\2", text)
-        elif keep == True and camel_case_split == True:
+        elif self.keep == True and self.do_camel_case_split == True:
             return re.sub(pattern, lambda x: f"{x.group()} {self.camel_case_split(x.group()[1:])}", text)
-        elif keep == False and camel_case_split == True:
+        elif self.keep == False and self.do_camel_case_split == True:
             return re.sub(pattern, lambda x: f"{self.camel_case_split(x.group()[1:])}", text)
         else:
             return re.sub(pattern, "\\2", text)
 
 
 class MentionHandler:
-    def __call__(self, text: str, repl: str = None, keep: bool = True) -> str:
+    def __init__(self, repl: str = None, keep: bool = True) -> None:
+        self.repl = repl
+        self.keep = keep
+
+    def __call__(self, text: str) -> str:
         pattern = "(@([^\s]+))"
-        if repl:
-            return re.sub(pattern, repl, text)
+        if self.repl:
+            return re.sub(pattern, self.repl, text)
         else:
-            if keep == True:
+            if self.keep == True:
                 return re.sub(pattern, "\\1 \\2", text)
             else:
                 return re.sub(pattern, "\\2", text)
 
 
 class PunctuationHandler:
-    def __init__(self) -> None:
+    def __init__(self, keep_set: set = set()) -> None:
         self.punct_set = set(string.punctuation + '''…'"`’”“''' + '️')
+        self.punct_set -= keep_set
 
     def __call__(self, text: str) -> str:
         cleaned_text = ""
@@ -81,3 +93,14 @@ class PunctuationHandler:
             if ch not in self.punct_set:
                 cleaned_text += ch
         return cleaned_text
+
+
+class ReplaceHandler:
+    def __init__(self, filters: Dict[str, str]) -> None:
+        self.filters = filters
+
+    def __call__(self, text: str) -> str:
+        output = text
+        for k, v in self.filters.items():
+            output = re.sub(k, v, output)
+        return output
