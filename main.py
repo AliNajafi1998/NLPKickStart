c73b90f0ce340d1602582ec8c199d10ba19696dd
AliNajafi1998 <najafi1998ali@gmail.com>
2022-08-15 13:52:06 +0300

    more samples

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pprint
+import html
 
 from NlpKickStart import (Pipeline, UnicodeNormalizer, EmojiHandler, RegexHandler, HashTagHandler, URLHandler,
                           HTMLHandler, MentionHandler, ReplaceHandler, PunctuationHandler, CaseFoldingNormalizer,
@@ -7,7 +8,9 @@ from NlpKickStart import (Pipeline, UnicodeNormalizer, EmojiHandler, RegexHandle
 sample_texts = [
     """They do this while filmed what do they do when they are not... we all know they murder! Defund the police 
     https://t.co/N1Vj0ACabb 😣 #BigMurder @CoderLone""",
-    """"Saint Paul&#39;s fixes cancer <br><a href=""https://jtvideos.blogspot.com/2022/05/saint-pauls-fixes-cancer.html"">https://jtvideos.blogspot.com/2022/05/saint-pauls-fixes-cancer.html </a><br>	During my PH D work at 
+    """"Saint Paul&#39;s fixes cancer 
+    <br><a href=""https://jtvideos.blogspot.com/2022/05/saint-pauls-fixes-cancer.html""></a><br>
+    During my PH D work at 
     Sheffield University 2000, we realised that ultrasound cause water to boil.  Welcome to molecular nuclear fusion.  
     For which professor Z refuses to take any credit.  Curious, an academic not waiting to take credit for the 
     students&#39; ideas. <br>1	H₂O+P+US→He+O+E #39;re doing biological molecular nuclear fusion. &amp;  The most 
@@ -15,9 +18,15 @@ sample_texts = [
     there is a 1.5kmx2cm partial steam plasma. <br>	I became aware that ultrasound scans of cancers, 
     cause the emission of X rays.  Of which there is no chemical source!  You& <br>2	H₂O+P+TU→E²+L...Z-ray		
     here E²=2.5x10³⁰ W"	HxHnIZfuDGs""",
-    'test test@gmail.com', "you're a big guy"]
+    'test test@gmail.com', "you're a big guy",
+    "&quot;The task of literature is to reconnect us with feelings, which might otherwise be unbearable to "
+    "study.&quot;&#39;",
+    "FAVORITE LIKE FROM KUBRICK&#39;S FULL METAL JACKET:  &quot;YOU&#39;RE SO UGLY YOU COULD BE A MODERN ART "
+    "MASTERPIECE!&quot;",
+    "👍🙏", "Sir Lieutenant will be pronounced as लेफ्टिनेंट not ल्यूटिनेंट"]
 
 pipeline = Pipeline([
+    html.unescape,
     UnicodeNormalizer(),
     EmailHandler(),
     EmojiHandler(),
@@ -28,13 +37,12 @@ pipeline = Pipeline([
     URLHandler(keep=False),
     MentionHandler(keep=False),
     RegexHandler(r'\s+', ' '),
-    ReplaceHandler(filters={'<br>': '', '&amp;': " and "}),
-    PunctuationHandler({'.', "'"}),
+    RegexHandler(r"[^a-zA-Z0-9\. ]", ' '),
     CaseFoldingNormalizer(),
     RegexHandler(r"\.+", "."),
-    RegexHandler("  +", " "),
+    RegexHandler(r"  +", " "),
+    str.strip,
     NLTKSentenceTokenizer()
-
 ])
 
 pprint.pprint(pipeline(sample_texts))
